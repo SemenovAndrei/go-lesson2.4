e86c1d20a3f96fd08aad5f295a121df77a86c1ce
Andrey Semenov <andss82@gmail.com>
2020-07-26 01:22:24 +0300

    add trace

--- cmd/card/main.go
+++ cmd/card/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
 
 	"github.com/i-hit/go-lesson2.4.git/pkg/card"
 )
 
 func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+	err = trace.Start(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer trace.Stop()
+
 	transaction := card.MakeTransactions()
 
 	fmt.Println("\n", card.GetMap(transaction, 1))
